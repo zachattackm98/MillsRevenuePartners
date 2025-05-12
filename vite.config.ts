ebfc2867f9b40e4bdd15110d96ca01b9c1956cca
zachattackm98 <zachm98@hotmail.com>
2025-05-11 23:15:43 -0700

    Update Vite configuration to resolve React Refresh error

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    include: ['react', 'react-dom'],
+    exclude: ['lucide-react']
   },
+  server: {
+    hmr: {
+      overlay: false
+    }
+  }
 });
