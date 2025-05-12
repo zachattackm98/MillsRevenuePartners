1c77ac3fda49eae4bc2431b88facf15d10e5ac92
zachattackm98 <zachm98@hotmail.com>
2025-05-11 22:47:06 -0700

    Add automatic scroll to top on page navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import { AnimatePresence } from 'framer-motion';
 
 const Layout: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
