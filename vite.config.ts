94cf658bb7e327e78cd4d11f1c6f5b982f4f637d
zachattackm98 <zachm98@hotmail.com>
2025-05-23 20:47:32 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/MillsRevenuePartners/',
   optimizeDeps: {
     include: ['react', 'react-dom'],
     exclude: ['lucide-react']
