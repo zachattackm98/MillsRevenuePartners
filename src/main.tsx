d87194fa15c2f1353e0d478f874927434d2699b8
zachattackm98 <zachm98@hotmail.com>
2025-05-23 20:14:54 -0700

    Updated package.json

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/MillsRevenuePartners">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
