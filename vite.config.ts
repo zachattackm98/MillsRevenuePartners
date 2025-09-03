3fff7920d58a6b73d99f3900f01b77aebfaea154
zachattackm98 <zachm98@hotmail.com>
2025-09-02 20:25:12 -0600

    Fix React Router context error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react', 'react-dom'],
+    include: ['react', 'react-dom', 'react-router-dom'],
     exclude: ['lucide-react']
   },
   server: {
